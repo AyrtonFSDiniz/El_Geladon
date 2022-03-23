8d36d0e6e8d664d5c72148ab79498876ab6b9857
AyrtonFSDiniz <ayrtondefreitassilva@gmail.com>
2022-03-23 20:56:10 -0300

    :construction: Implementando front e back

--- index.js
+++ index.js
@@ -2,49 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const port = 3000;
 const app = express();
-app.use(express.json());
+const route = require('./src/routes/paletas.route');
+
 app.use(cors());
+app.use(express.json());
 
-const paletas = [
-  {
-    id: 1,
-    sabor: 'Açaí com Leite Condensado',
-    descricao:
-      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
-    foto: 'assets/images/acai-com-leite-condensado.png',
-    preco: 10.0,
-  },
-  {
-    id: 2,
-    sabor: 'Banana com Nutella',
-    descricao:
-      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
-    foto: 'assets/images/banana-com-nutella.png',
-    preco: 10.0,
-  },
-  {
-    id: 3,
-    sabor: 'Chocolate Belga',
-    descricao:
-      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
-    foto: 'assets/images/chocolate-belga.png',
-    preco: 7.0,
-  },
-];
-
-app.get('/', function (req, res) {
-  res.send('Tudo OK com a aplicação');
-});
-
-app.get('/paletas', function (req, res) {
-  res.send(paletas);
-});
-
-app.get('/paletas/:id', function (req, res) {
-  const idParam = req.params.id;
-  const paletaUni = paletas.find((paleta) => paleta.id == idParam);
-  res.send(paletaUni);
-});
+app.use('/paletas', route);
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
---
+++ src/controllers/paletas.controller.js
@@ -0,0 +1,51 @@
+const paletasService = require('../services/paletas.service');
+
+const findPaletasController = (req, res) => {
+  const allPaletas = paletasService.findPaletasService();
+  res.send(allPaletas);
+};
+
+const findPaletaByIdController = (req, res) => {
+  const idParam = req.params.id;
+  const chosenPaleta = paletasService.findPaletaByIdService(idParam);
+  res.send(chosenPaleta);
+};
+
+const createPaletaController = (req, res) => {
+  const paleta = req.body;
+  if (
+    !paleta ||
+    !paleta.sabor ||
+    !paleta.descricao ||
+    !paleta.foto ||
+    !paleta.preco
+  ) {
+    return res.send(
+      'Você não preencheu todos os dados para adicionar uma nova paleta ao cardápio!',
+    );
+  }
+
+  const newPaleta = paletasService.createPaletaService(paleta);
+  res.send(newPaleta);
+};
+
+const updatePaletaController = (req, res) => {
+  const idParam = +req.params.id;
+  const paletaEdit = req.body;
+  const updatedPaleta = paletasService.updatePaletaService(idParam, paletaEdit);
+  res.send(updatedPaleta);
+};
+
+const deletePaletaController = (req, res) => {
+  const idParam = req.params.id;
+  paletasService.deletePaletaService(idParam);
+  res.send({ message: 'Paleta deletada com sucesso!' });
+};
+
+module.exports = {
+  findPaletasController,
+  findPaletaByIdController,
+  createPaletaController,
+  updatePaletaController,
+  deletePaletaController,
+};
